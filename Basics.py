fdc6a6978568377f13b90e84d70d2f790afbdeb4
Samuel Ahumuza <samuelahumuza15@gmail.com>
2025-06-11 13:01:55 +0300

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,14 @@
+# <- single line comment
+"""
+Multi line comment
+this is another commented line
+"""
+# print("this is my name") ctrl + /
+# Pyhton coding Case (Snake case)
+"""
+first_name
+last_name
+"""
+#indent
+first_name = "samuel"
+print(first_name)
