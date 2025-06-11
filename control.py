2030ee52827a5d1c463ac983cd86b576e78c03fa
Samuel Ahumuza <samuelahumuza15@gmail.com>
2025-06-11 12:56:51 +0300

    Add files via upload
        A PYTHON PROGRAM ALLOWING AN INSTRUCTOR TO ENTER A MARK STRICTLY BTN 0 & 100 THEN SHOWING GRADES.

---
+++ control.py
@@ -0,0 +1,27 @@
+'''
+
+    A PYTHON PROGRAM ALLOWING AN INSTRUCTOR TO ENTER A MARK STRICTLY BTN 0 & 100 THEN SHOWING GRADES.
+    '''
+print("wecome to the grading scale")
+"mark"  (input ("please enter mark between 0 and 100"))
+
+if  "mark" >=100:
+    print("A+")
+elif "mark" >=90:
+    print("A")
+elif "mark" >=80:
+    print("B+")
+elif "mark" >=70:
+    print("B")
+elif "mark" >=60:
+    print("C+")
+elif "mark" >=50:
+    print("C")
+elif "mark" >=40:
+    print("D+")
+elif "mark" >=30:
+    print("D")
+elif "mark" >=20:
+    print("F")
+else:
+    print("invalid, please enter mark")
