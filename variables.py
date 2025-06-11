424f08e37d1b050262e754c09165ac3df412c5f8
Samuel Ahumuza <samuelahumuza15@gmail.com>
2025-06-11 13:00:29 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,29 @@
+## value
+## data type
+"""
+intergers
+floats
+strings/characters
+Boolean
+"""
+number_one = 30 # interger
+number_two = 3.17 # float
+number_three = "Samuel" # string
+is_attentive = False
+is_prensent = True
+
+## checking for datatype
+print(type(number_one))
+print (type(number_two))
+# convert the integer to a string
+my_integer = 30
+my_string = str(my_integer)
+print(f"Original integer:{my_integer}(Type:{type(my_string)}")
+#print(converted string:{my_string} Type:{type(my string)})
+
+print(f"integer: {number_two} datatype:{type(number_two)}")
+#integer "8"
+#convert the integer to a string
+string_int = "8"
+string_int = int(string_int)
+print(string_int)
