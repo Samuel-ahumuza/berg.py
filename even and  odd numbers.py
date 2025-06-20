741123c3e28943ac28f53f48516aaf46b2f56dba
Samuel Ahumuza <samuelahumuza15@gmail.com>
2025-06-20 15:01:00 +0300

    Update even and  odd numbers.py

--- even and  odd numbers.py
+++ even and  odd numbers.py
@@ -1,12 +1,11 @@
 def main(): 
     print("Welcome to the Indentation Race!")
-    print("Welcome to the Indentation Race!") 
 for i in range(5): 
     print(f"Current number: {i}") 
-    if i % 2 == 0: 
-       print("Even number.",) 
-    else:
-       print("Odd number.",)
-print("Race completed!") 
+if i % 2 == 0: 
+    print("Even number.",) 
+else:
+    print("Odd number.",)
+    print("Race completed!") 
  
 main() 
